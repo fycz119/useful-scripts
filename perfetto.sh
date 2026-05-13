760c2d45a4a5c7676ea3db682baeda33111ea5e0
Zhi <1198117429@qq.com>
2026-05-13 18:39:42 +0800

    Update perfetto.sh

--- perfetto.sh
+++ perfetto.sh
@@ -2,3 +2,5 @@ adb shell perfetto \
 -o /data/misc/perfetto-traces/systrace.pftrace \
 -t 15s \
 sched freq idle memory disk workq power binder_driver
+
+adb shell simpleperf stat -p <PID> -e l1d_cache_refill,l2d_cache_refill,bus_access --duration 5
